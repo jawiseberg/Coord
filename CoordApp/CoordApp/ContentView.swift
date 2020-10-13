6ada8bc50ad927cd2111226973422a539cc31979
Jake Wiseberg <jawiseberg@gmail.com>
2020-10-13 19:47:53 -0400

    initialize swift project (using xcode)

---
+++ CoordApp/CoordApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoordApp
+//
+//  Created by Jake Wiseberg on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoordApp/CoordApp/CoordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordAppApp.swift
+//  CoordApp
+//
+//  Created by Jake Wiseberg on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct CoordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
