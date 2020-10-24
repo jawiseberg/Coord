d04826a75e0daac14685f56a3d0086a40c5d522a
bek44 <bek44@case.edu>
2020-10-24 11:26:07 -0400

    assignment getters and setters

---
+++ CoordApp/CoordApp/Assignment.swift
@@ -0,0 +1,65 @@
+//
+//  Assignment.swift
+//  CoordApp
+//
+//  Created by Bailey Kaplan on 10/24/20.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct Assignments {
+    
+    var iD: Int = 0
+    var title: String = ""
+    var dueDate: Int = 0 //how do we express date best
+    var dueTime: Int = 0 //how do we express time best
+    var course: String = ""
+    var state: String = ""
+    
+    func getID()-> Int {
+        return iD
+    }
+    
+    func getTitle()-> String {
+        return title
+    }
+    
+    func setTitle(newTitle: String)-> String {
+        return newTitle
+    }
+    
+    func getDueDate()-> Int {
+        return dueDate
+    }
+    
+    func setDueDate(newDueDate: Int)-> Int {
+        return newDueDate
+    }
+    
+    func getDueTime()-> Int {
+        return dueTime
+    }
+    
+    func setDueTime(newDueTime: Int)-> Int {
+        return newDueTime
+    }
+    
+    func getCourse()-> String {
+        return course
+    }
+    
+    func setCourse(newCourse: String)-> String {
+        return newCourse
+    }
+    
+    func getState()-> String {
+        return state
+    }
+    
+    func setState(newState: String)-> String {
+        return newState
+    }
+    
+ }
