ba30487b743a7b6e4782e4d205bfdebd1dd11573
Takoda Denhof <tdenhof16@gmail.com>
2020-11-14 15:25:14 -0500

    Assignment Files

---
+++ Coord/Coord/AssignmentFiles.swift
@@ -0,0 +1,45 @@
+//
+//  AssignmentFiles.swift
+//  Coord
+//
+//  Edited by Takoda Denhof on 11/14/20.
+//  AssignmentProject
+//
+//  Created by Bailey Kaplan on 11/6/20.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct assignment : Identifiable {
+    
+    var id = String()
+    var assignmentName = String()
+    var assignmentCourse = String()
+    var assignmentDueDate = String()
+    
+}
+struct classes : Identifiable {
+    var id = String()
+    var className = String()
+    var type = String()
+    var assignmentList = AssignmentStore()
+}
+
+class AssignmentStore : ObservableObject {
+    @Published var assignments = [assignment]()
+    
+}
+class ClassAssignments : ObservableObject {
+    @Published var classAss = [classes]()
+}
+class rowView : ObservableObject {
+    
+}
+
+struct TaskFIles_Previews: PreviewProvider {
+    static var previews: some View{
+        Text("Hello World")
+    }
+}
--- Coord/Coord/Assignments.swift
+++ Coord/Coord/Assignments.swift
@@ -1,64 +1,190 @@
 //
 //  Assignments.swift
 //  Coord
+////  Created by Bailey Kaplan on 11/4/20.
 //
-//  Created by Bailey Kaplan on 10/24/20.
+//  Edited by Takoda Denhof on 11/14/20.
 //
 
 import Foundation
-import UIKit
+import SwiftUI
+import CoreData
+import Combine
+// Class List
+struct AssignmentHome : View {
+    @ObservedObject var classStore = ClassAssignments()
+    @State var name = String()
+    
+    func newClass(){
+        classStore.classAss.append(classes(id : "", className: self.name, assignmentList: AssignmentStore() ))
+        self.name = ""
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+            HStack{
+            TextField("Classes", text : self.$name)
+            Button(action : newClass, label: {Text("Add List")})
+            }
+            VStack{
+            List{
+                ForEach(self.classStore.classAss){ classAss in NavigationLink(destination : AssignmentView()){
+                    Text(classAss.className)
+            }}.onMove(perform: self.move)
+                .onDelete(perform: self.delete)
+            }.padding(.all).navigationBarTitle("Assignment Lists")
+        .navigationBarItems(trailing: EditButton())
+        }}
+    }
+    }
+    // Move data within the array to allow visualiztion. Swaps Index's
+    func move(from source : IndexSet, to destination : Int) {
+        self.classStore.classAss.move(fromOffsets: source, toOffset: destination)
+    }
+    // Deleting Row
+    func delete(at offsets : IndexSet) {
+        self.classStore.classAss.remove(atOffsets: offsets)
+    }
+}
 
-struct Assignments {
+
+// List of Specific Assignment Based on Classes you chose from
+struct AssignmentView : View {
+    @ObservedObject var assignmentStore = AssignmentStore()
+    @State var newAssignment : String = ""
+    @State var name = ""
+    @State var type = ""
+    @State var dueDate = ""
+    @State var nameRetrieved = ""
+    @State var courseRetrieved = ""
+    @State var dueDateRetrieved = ""
+    var assignmentTypes = ["Homework","Project","Notes","Study","Exam"]
     
-    var iD: Int = 0
-    var title: String = ""
-    var dueDate: Int = 0 //how do we express date best
-    var dueTime: Int = 0 //how do we express time best
-    var course: String = ""
-    var state: String = ""
+
+    // View to Add Assignments
+    var addAssignment : some View{
+    NavigationView{
+            List{
+        VStack{
+        
+        Text("Assignments")
+            .font(.title)
     
-    func getID()-> Int {
-        return iD
+        TextField("Enter the name of your assignment", text: $name).textFieldStyle(RoundedBorderTextFieldStyle())
+        
+        Picker(selection : $type, label : Text("Select Type")){
+                
+                ForEach(0 ..< assignmentTypes.count){Text(String(self.assignmentTypes[$0]))
+                
+                }
+            
+            }.padding()
+        
+        TextField("When is your assignment due?", text: $dueDate).textFieldStyle(RoundedBorderTextFieldStyle())
+    }
+    }
+    }
     }
     
-    func getTitle()-> String {
-        return title
+// Body View
+    var body: some View {
+        NavigationView{
+            List{
+            HStack {
+            NavigationLink(destination : addAssignment, label : {Text("Add Assignment")})}
+
+                VStack {
+                    List {
+                        ForEach(self.assignmentStore.assignments) { task in
+                            Text(task.assignmentName)
+                        }.onMove(perform: self.move)
+                            .onDelete(perform: self.delete)
+                            }.padding(.all).navigationBarTitle("Assignments")
+                    .navigationBarItems(trailing: EditButton())
+                }
+        }
+
+    }
     }
     
-    func setTitle(newTitle: String)-> String {
-        return newTitle
+    func addNewAssignmentfunc() {
+        assignmentStore.assignments.append(assignment(id: "" , assignmentName: self.name, assignmentCourse: self.type, assignmentDueDate: self.dueDate))
+        self.newAssignment = ""
     }
     
-    func getDueDate()-> Int {
+    /*
+    func getName(iD: String)-> String {
+        //@ObservedObject var tempTaskStore = TaskStore()
+        //if let tempTask = taskStore[iD] as? [String: Any]{
+        //return tempTask.assignmentName
+    }
+ */
+    
+    func getCourse()-> String {
+        return type
+    }
+    
+    func getDueDate()-> String {
         return dueDate
     }
     
-    func setDueDate(newDueDate: Int)-> Int {
-        return newDueDate
-    }
     
-    func getDueTime()-> Int {
-        return dueTime
+    func move(from source : IndexSet, to destination : Int) {
+        assignmentStore.assignments.move(fromOffsets: source, toOffset: destination)
     }
-    
-    func setDueTime(newDueTime: Int)-> Int {
-        return newDueTime
+
+    func delete(at offsets : IndexSet) {
+        assignmentStore.assignments.remove(atOffsets: offsets)
     }
-    
-    func getCourse()-> String {
-        return course
+
+        
+}
+
+
+struct AssignmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AssignmentHome()
     }
-    
-    func setCourse(newCourse: String)-> String {
-        return newCourse
+}
+
+struct DropDown : View {
+    @State var expand = false
+    var body: some View {
+        VStack() {
+            VStack(spacing: 30){
+                HStack(){
+                    Text("Assignment Completion")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.black)
+                    Image(systemName: expand ? "chevron.up" : "chevron.down")
+                        .resizable()
+                        .frame(width: 13, height: 6)
+                        .foregroundColor(.black)
+                }.onTapGesture{
+                    self.expand.toggle()
+                }
+                if expand {
+                    Button(action: {
+                        print("1")
+                        self.expand.toggle()
+                    }) {
+                        Text("Not Started")
+                    }.foregroundColor(.black)
+                    Button(action: {
+                        print("2")
+                        self.expand.toggle()
+                    }){
+                        Text("In Progress")
+                    }.foregroundColor(.black)
+                    Button(action: {
+                        print("3")
+                        self.expand.toggle()
+                    }){
+                        Text("Completed")
+                }.foregroundColor(.black)
+            }
+        }
     }
-    
-    func getState()-> String {
-        return state
     }
-    
-    func setState(newState: String)-> String {
-        return newState
-    }
-    
- }
+}
